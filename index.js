0bd82adb66884913d9f1184ed32adbeebd0dc70f
ragu0115 <raghu.palani01@gmail.com>
2024-03-31 00:08:45 -0700

    more js funcs w errors gotta figure it out

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
+// Function for start element
 function start() {
     var target = document.getElementById('blue');
     target.style.display = 'none';
     document.getElementById('red').style.display = 'inline';
   }
 
+// Building Reaction time test
+
+// Function if element is pressed early
+function early() {
+  var target = document.getElementById('red');
+  target.style.display = 'none';
+  document.getElementById('yellow').style.display = 'inline';
+}
+
+// Function if element is pressed on time
+function onTime() {
+  var target = document.getElementById('red');
+  target.style.display = 'none';
+      document.getElementById('green').style.display = 'inline';
+}
+
+document.getElementById("red").addEventListener("click", function() {
+  var delay = Math.floor(Math.random() * 4000) + 1000;
+}
+
 function wait() {
     var target = document.getElementById('red');
-    target.style.display = 'none';
-    startTime = new Date().getTime();
+    var delay = Math.random() * 4000 + 1000;
+    var timer = setTimeout(onTime, delay);
   }
 
-function start2() {
-    var target = document.getElementById('blue');
+function click() {
+    var target = document.getElementById('green');
     target.style.display = 'none';
-    target.style.top = Math.random() * (window.innerHeight - 100) + 'px';
-    target.style.left = Math.random() * (window.innerWidth - 100) + 'px';
     startTime = new Date().getTime();
   }
 
