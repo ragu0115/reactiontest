a541a502e864b5789c1bcc132905ec5f4a889b93
ragu0115 <raghu.palani01@gmail.com>
2024-03-31 20:56:02 -0700

    update

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ function early() {
   document.getElementById('yellow').style.display = 'inline';
 }
 
+// Function if we reach the early click element
+function reset() {
+  document.getElementById('yellow').style.display = 'none';
+  document.getElementById('blue').style.display = 'inline';
+}
+
 // Function if element is pressed on time
 function onTime() {
   var target = document.getElementById('red');
   target.style.display = 'none';
       document.getElementById('green').style.display = 'inline';
-}
-
-document.getElementById("red").addEventListener("click", function() {
-  var delay = Math.floor(Math.random() * 4000) + 1000;
 }
 
 function wait() {
@@ -37,10 +39,3 @@ function click() {
     startTime = new Date().getTime();
   }
 
-function start3() {
-    var target = document.getElementById('blue');
-    target.style.display = 'none';
-    target.style.top = Math.random() * (window.innerHeight - 100) + 'px';
-    target.style.left = Math.random() * (window.innerWidth - 100) + 'px';
-    startTime = new Date().getTime();
-  }
