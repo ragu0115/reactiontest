be442b8c17946eee8988cd5de7fdcc4d1af2fcbf
ragu0115 <raghu.palani01@gmail.com>
2024-03-31 22:26:35 -0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+var timer, startTime;
+
+
+// Function if element is pressed on time
+function onTime() {
+  var target = document.getElementById('red');
+  target.style.display = 'none';
+      document.getElementById('green').style.display = 'inline';
+      startTime = new Date().getTime();
+}
+
 // Function for start element
 function start() {
     var target = document.getElementById('blue');
     target.style.display = 'none';
     document.getElementById('red').style.display = 'inline';
+    var delay = Math.random() * 4000 + 1000;
+    timer = setTimeout(onTime, delay);
   }
 
 // Building Reaction time test
 
 // Function if element is pressed early
 function early() {
+  clearTimeout(timer);
   var target = document.getElementById('red');
   target.style.display = 'none';
   document.getElementById('yellow').style.display = 'inline';
@@ -20,22 +34,9 @@ function reset() {
   document.getElementById('blue').style.display = 'inline';
 }
 
-// Function if element is pressed on time
-function onTime() {
-  var target = document.getElementById('red');
-  target.style.display = 'none';
-      document.getElementById('green').style.display = 'inline';
-}
-
-function wait() {
-    var target = document.getElementById('red');
-    var delay = Math.random() * 4000 + 1000;
-    var timer = setTimeout(onTime, delay);
-  }
-
 function click() {
-    var target = document.getElementById('green');
-    target.style.display = 'none';
-    startTime = new Date().getTime();
-  }
-
+  document.getElementById('green').style.display = 'none';
+  document.getElementById('white').style.display = 'inline';
+  var endTime = new Date().getTime();
+  var reactionTime = (endTime - startTime) / 1000;
+}
